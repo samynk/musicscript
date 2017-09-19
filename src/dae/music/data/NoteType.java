a249100f7b21d780b2b82ee3f9533288fe6d780d
Koen.Samyn <Koen.Samyn@DFR9R72.hogeschool-wvl.be>
2017-09-19 13:37:55 +0200

    Fixed error with 32nd note type.

--- src/dae/music/data/NoteType.java
+++ src/dae/music/data/NoteType.java
@@ -6,7 +6,7 @@ package dae.music.data;
  */
 public enum NoteType {
     D1024(1,256,"1024th"),D512(1,128,"512th"),D256(1,64,"256th"),D128(1,32,"128th"),
-    D64(1,16,"64th"),D32(1,8,"32th"),SIXTEENTH(1,4,"16th"),EIGHT(1,2,"eighth"),
+    D64(1,16,"64th"),D32(1,8,"32nd"),SIXTEENTH(1,4,"16th"),EIGHT(1,2,"eighth"),
     QUARTER(1,1,"quarter"),
     HALF(2,1,"half"),WHOLE(4,1,"whole"),BREVE(8,1,"breve"),LONG(16,1,"long"),MAXIMA(32,1,"maxima");
     private final int numerator;
